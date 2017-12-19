6c4e6b49348a0f2b448469493b39237854dd342a
Arun PM <arunpm@gmail.com>
2017-12-19 16:38:52 +0530

    License update.

--- app_debug.h
+++ app_debug.h
@@ -1,8 +1,6 @@
 /*
  * app_debug.h
  *
- * Copyright 2017 vvdnlt241 <arun.pm@vvdntech.in>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
